4c6d9efd0e035864382bdd0d25d0034a4cd401e3
Your Name <you@example.com>
2025-12-09 08:48:13 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,36 @@
+let fun=function(name,pwd){
+    console.log("hello");
+    console.log(`username:${name}, pwd:${pwd}`);
+    return {
+        name:"haa",
+        pwd:"124",
+    }
+    
+}
+
+console.log(fun("haar","123"));
+/*normalfunction
+function getherDetails(){
+  console.log("this is normal function");
+  return 
+}
+console.log(gatherDetails());
+console.log(gatherDetails());*/
+
+//generator func
+/*function* gen(){
+    yield a=10;
+    yield b=20;
+    yield c=30;
+console.log("gen func");
+}
+let result=gen();
+console.log(result.next().value);
+console.log(result.next().value);
+console.log(result.next().value);*/
+
+
+
+
+
+
